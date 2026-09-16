6f1a505a84a704e77402c1387dbeca8d7bd76e86
Arko <talhasuhayeb@gmail.com>
2026-09-16 23:48:42 +0600

    Change the reset email template.

---
+++ HMS-app/resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            <span style="display: inline-block; margin-right: 8px; padding: 8px 12px; border-radius: 10px; background: #2cb1bc; color: #102a43; font-size: 18px; font-weight: 800;">+</span>
+            eAppointment Care
+        </a>
+    </td>
+</tr>
